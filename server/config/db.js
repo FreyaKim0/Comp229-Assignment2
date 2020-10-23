df819b04b4b6eaec91c91d5ba1d914445ba0f98f
Jin <ogataakiiiii@gmail.com>
2020-10-23 17:16:43 -0400

    update URI DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/book_store"
+    //"URI":"mongodb://localhost/book_store"
+    "URI":"mongodb+srv://freyakim:popo2929@mongodbserver.aj3jp.mongodb.net/book_store?retryWrites=true&w=majority"
 }
