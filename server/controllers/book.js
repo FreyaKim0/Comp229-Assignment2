0fef3a8fbfb431d9a91e10fb3d43113b8ca9731d
Jin <ogataakiiiii@gmail.com>
2021-01-14 11:05:43 -0500

    controller-book.js

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -1,112 +1,134 @@
-let express=require('express');
+let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-//create a reference to the model
-let Book=require('../models/book');
+let jwt = require('jsonwebtoken');
 
-module.exports.displayBookList = (req,res,next)=>{
-    Book.find((err,bookList)=>{
-        if(err){
+// create a reference to the model
+let Book = require('../models/book');
+
+module.exports.displayBookList = (req, res, next) => {
+    Book.find((err, bookList) => {
+        if(err)
+        {
             return console.error(err);
         }
-        else{
-           //console.log(bookList);
-           
-           /*
-           res.render('art/list', {title:'BUSINESS CONTACT',
-           BookList:bookList,
-           displayName:req.user ? req.user.displayName:''});
-           */
-          res.json(bookList);
+        else
+        {
+            //console.log(BookList);
+
+            /*
+            res.render('book/list', 
+            {title: 'Books', 
+            BookList: bookList, 
+            displayName: req.user ? req.user.displayName : ''});      
+            */
+
+            res.json(bookList);
         }
-     });
-   }
+    });
+}
 
-   module.exports.displayAddPage = (req,res,next)=>{
- 
+module.exports.displayAddPage = (req, res, next) => {
     /*
-    res.render('art/add', {title:'ADD CONTACT',
-    displayName:req.user ? req.user.displayName:''})
+    res.render('book/add', {title: 'Add Book', 
+    displayName: req.user ? req.user.displayName : ''});
     */
-
-    res.json({success:true,msg:'Successfully displayed add page'});
+   
+    res.json({success: true, msg: 'Succesfully Displayed Add Page'});
 }
 
-   module.exports.processAddPage = (req,res,next)=>{
+module.exports.processAddPage = (req, res, next) => {
     let newBook = Book({
-        "name":req.body.name,
-        "author":req.body.author,
-        "published":req.body.published,
-        "description":req.body.description
+        "name": req.body.name,
+        "author": req.body.author,
+        "published": req.body.published,
+        "description": req.body.description,
+        "price": req.body.price
     });
 
-    Book.create(newBook,(err,Book)=>{
-       if(err){
-           console.log(err);
-           res.end(err);
-       }
-       else{
-           //refresh the book list
-           //res.redirect('/book-list');
-
-           res.json({success:true,msg:'Successfully added new book'});
-       }
-    });
-}
-
-module.exports.displayEditPage =(req,res,next)=>{
-    let id = req.params.id;
-    Book.findById(id,(err,bookToEdit)=>{
+    Book.create(newBook, (err, Book) =>{
         if(err)
         {
             console.log(err);
             res.end(err);
         }
-        else{
-            //show the edit
-            /*res.render('art/edit',{title:'EDIT CONTACT',displayName:req.user ? req.user.displayName:'',book: bookToEdit})*/
-            res.json({success:true,msg:'Successfully Displayed Book to Edit,book:bookToEdit'});
+        else
+        {
+            // refresh the book list
+            //res.redirect('/book-list');
+
+            res.json({success: true, msg: 'Successfully Added New Book'});
+        }
+    });
+
+}
+
+module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    Book.findById(id, (err, bookToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            /*
+            res.render('book/edit', {title: 'Edit Book', book: bookToEdit, 
+            displayName: req.user ? req.user.displayName : ''});
+            */
+
+            res.json({success: true, msg: 'Successfully Displayed Book to Edit', book: bookToEdit});
         }
     });
 }
 
-module.exports.processEditPage=(req,res,next)=>{
-    let id=req.params.id;
-    let updatedBook= Book({
-        "_id":id,
-        "name":req.body.name,
-        "author":req.body.author,
-        "published":req.body.published,
-        "description":req.body.description
+module.exports.processEditPage = (req, res, next) => {
+    let id = req.params.id
+
+    let updatedBook = Book({
+        "_id": id,
+        "name": req.body.name,
+        "author": req.body.author,
+        "published": req.body.published,
+        "description": req.body.description,
+        "price": req.body.price
     });
-    Book.updateOne({_id:id},updatedBook,(err)=>{
-        if(err){
+
+    Book.updateOne({_id: id}, updatedBook, (err) => {
+        if(err)
+        {
             console.log(err);
             res.end(err);
         }
-        else{
-            //refresh the book list
+        else
+        {
+            // refresh the book list
             //res.redirect('/book-list');
-            
-            res.json({success:true,msg:'Successfully Edited Book',book: updatedBook});
+
+            res.json({success: true, msg: 'Successfully Edited Book', book: updatedBook});
         }
     });
 }
 
-module.exports.performDelete = (req,res,next)=>{
-    let id=req.params.id;
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
 
-    Book.remove({_id:id},(err)=>{
-        if(err){
+    Book.deleteOne({_id: id}, (err) => {
+        if(err)
+        {
             console.log(err);
             res.end(err);
         }
-        else{
-            //refresh the book list
-            //res.redirect('/book-list');
+        else
+        {
+             // refresh the book list
+             //res.redirect('/book-list');
 
-            res.json({success:true,mdg:'successfully deleted book'});
+             res.json({success: true, msg: 'Successfully Deleted Book'});
         }
     });
 }
