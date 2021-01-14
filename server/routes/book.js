9942f4caf80763360f7ac0c0c62f442f2544f228
Jin <ogataakiiiii@gmail.com>
2021-01-14 10:55:16 -0500

    sroutes/book.js

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,36 +1,40 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
-let passport = require('passport');
-let bookController = require('../controllers/book');
-const book = require('../models/book');
 
-//helper function for guard purpose
-function requireAuth(req,res,next){
-  //check if the user is logged in
-  if(!req.isAuthenticated())
-  {
-      return res.redirect('/login');
-  }
-  next();
+let jwt = require('jsonwebtoken');
+
+let passport = require('passport');
+
+let bookController = require('../controllers/book');
+
+// helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    // check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
 }
 
-/* GET Route for the book list page - read operation */
-router.get('/',bookController.displayBookList);
+/* GET Route for the Book List page - READ Operation */
+router.get('/', bookController.displayBookList);
 
-/* Get Route for the ADD page- CREATE operation */
-//router.get('/add', requireAuth,bookController.displayAddPage);
+/* GET Route for displaying the Add page - CREATE Operation */
+//router.get('/add', requireAuth, bookController.displayAddPage);
 
-/* POST Route for processing the Add page- CREATE operation */
-router.post('/add', passport.authenticate('jwt',{session: false}),bookController.processAddPage);
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add', passport.authenticate('jwt', {session: false}), bookController.processAddPage);
 
-/* Get Route for the EDIT page-  UPDAGE operation */
-//router.get('/edit/:id', requireAuth,bookController.displayEditPage);
+/* GET Route for displaying the Edit page - UPDATE Operation */
+//router.get('/edit/:id', requireAuth, bookController.displayEditPage);
 
-/* POST Route for processing the EDIT page- UPDAGE operation*/
-router.post('/edit/:id', passport.authenticate('jwt',{session: false}),bookController.processEditPage);
+/* POST Route for processing the Edit page - UPDATE Operation */
+router.post('/edit/:id', passport.authenticate('jwt', {session: false}), bookController.processEditPage);
 
-/* Get to perform Deletion- DELETE operation*/
-router.get('/delete/:id', passport.authenticate('jwt',{session: false}),bookController.performDelete);
+/* GET to perform  Deletion - DELETE Operation */
+router.get('/delete/:id', passport.authenticate('jwt', {session: false}), bookController.performDelete);
 
-module.exports=router;
+module.exports = router;
